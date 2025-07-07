0759eb33da671098bf6aacfd3fea0de7d2991bf7
amanntripathii <153132388+amanntripathii@users.noreply.github.com>
2025-07-07 18:15:31 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Money Management Tool',
+  description: 'Managing ur money....',
+  generator: 'Aman',
 }
 
 export default function RootLayout({
